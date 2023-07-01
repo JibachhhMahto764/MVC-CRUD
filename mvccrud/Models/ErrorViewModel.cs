1afd633e3b8466a8b23f32c536bcacbba5dcf202
jibachh <jibachhmahato@gmail.com>
2023-07-01 17:46:25 +0545

    Add project files.

---
+++ mvccrud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvccrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
